196bc1818b2f7dae1c1b828897c9a9bca1f50a8f
Nicholo Pardines <nick123abl@gmail.com>
2024-01-07 19:16:02 +0800

    Added more specific code, need to make an install template just for my laptop

--- arandr_i3.sh
+++ arandr_i3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 144 --pos 1366x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --mode 1366x768 --pos 0x428 --rotate normal
+xrandr --output DP-1 --mode 1920x1080 --rate 144 --pos 1366x0 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1366x768 --pos 0x307 --rotate normal
--- create_i3_poly_symlinks.sh
+++ create_i3_poly_symlinks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 mkdir -p ~/.config/alacritty/
-echo "ln -s $(pwd)/alacritty/alacritty.yml ~/.config/alacritty/"
-ln -s $(pwd)/alacritty/alacritty.yml ~/.config/alacritty/
+echo "ln -s $(pwd)/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml"
+ln -s $(pwd)/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 mkdir -p ~/.config/i3/config
-echo "ln -s $(pwd)/i3/config ~/.config/i3/"
-ln -s $(pwd)/i3/config ~/.config/i3/
+echo "ln -s $(pwd)/i3/config ~/.config/i3/config"
+ln -s $(pwd)/i3/config ~/.config/i3/config
 
 mkdir -p ~/.config/polybar/
 echo "ln -s $(pwd)/polybar/polybar_config ~/.config/polybar/config.ini"
