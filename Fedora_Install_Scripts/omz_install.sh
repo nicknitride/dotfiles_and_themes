09844314f7b8ce8bc12ca65fbcda3115eda98ed9
Nicholo Pardines <nick123abl@gmail.com>
2024-01-08 19:36:31 +0800

    Deleted old wallpapers

--- Fedora_Install_Scripts/omz_install.sh
+++ Fedora_Install_Scripts/omz_install.sh
@@ -3,3 +3,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+cd ~/dotfiles_and_themes/
+rm ../.zshrc
+cp ./OMZ/.zshrc ~/
+cp ./OMZ/lsd_alias.zsh $(ZSH_CUSTOM)/
