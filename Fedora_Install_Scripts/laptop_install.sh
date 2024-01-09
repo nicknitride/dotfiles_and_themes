812b310ef74353cd3556c24a4366dc9c0a864759
Nicholo Pardines <nick123abl@gmail.com>
2024-01-09 13:17:28 +0800

    Added laptop specific configuration

--- Fedora_Install_Scripts/laptop_install.sh
+++ Fedora_Install_Scripts/laptop_install.sh
@@ -14,9 +14,9 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 sudo dnf install code
 
-#* Add Font Install
-#* Add Theme Install
-#* Add Laptop Symlink Install (i3, polybar, etc.)
 
 flatpak install com.obsproject.Studio com.obsproject.Studio.Plugin.GStreamerVaapi com.obsproject.Studio.Plugin.Gstreamer  
 
+### Install Fusuma (Touch Gestures)
+sudo dnf install gem
+sudo gem install fusuma
