19bc9782c0ab834b9991b67c68c3120fb7646af6
Nicholo Pardines <nick123abl@gmail.com>
2024-01-10 21:58:30 +0800

    Added a universal config copy script

--- universal_config_copier.sh
+++ universal_config_copier.sh
@@ -42,22 +42,39 @@ function homeSymLink {
 
 
 
-homeSymLink alacritty alacritty.toml alacritty.toml
+# homeSymLink alacritty alacritty.toml alacritty.toml
 
-APPSINCLUDED=("alacritty" "polybar" "i3" "fusuma")
+
+#* From online: To find the length of an array in Bash, you can use the ${#array[@]} syntax. This command will return the number of elements in the array.
+function homeSymArrayCall {
+    local -n INTERNALAPPARRAY=${1}
+    local -n INTERNALFNAME=${2}
+    local -n INTERNALDESTFNAMEARRAY=${3}
+    ARRAYLENGTH=${#INTERNALAPPARRAY[@]}
+    for ((i = 0; i < ARRAYLENGTH; i++ ))
+        do  
+            homeSymLink "${INTERNALAPPARRAY[$i]}" "${INTERNALFNAME[$i]}" "${INTERNALDESTFNAMEARRAY[$i]}"
+    done
+}
+
+APPSINCLUDED=("alacritty" "polybar" "i3")
 # Helps us navigate both the dotfile and config folders
-INTERNALFILENAMEARRAY=("alacritty.toml" "polybar_config" "config" "config.yml")
+INTERNALFILENAMEARRAYDESKTOP=("alacritty.toml" "polybar_config" "desktop_config_i3")
+INTERNALFILENAMEARRAYLAPTOP=("alacritty.toml" "latop_config" "laptop_config")
 # Filenames used inside the dotfiles folder
-DESTINATIONFILENAMEARRAY=("alacritty.toml" "config" "config" "config")
+DESTINATIONFILENAMEARRAY=("alacritty.toml" "config" "config.ini")
 # Filenames used in the actual config folder
 
-# * From online: To find the length of an array in Bash, you can use the ${#array[@]} syntax. This command will return the number of elements in the array.
-# function homeSymArrayCall {
-#     for ITEM in "${APPSINCLUDED}"
-#         do
-#         echo "$ITEM"
-        
-#     done
-# }
-
-# homeSymArrayCall
+read -p "Enter type [desktop/laptop] : " USERANSWERTYPE
+    case $USERANSWERTYPE in
+    [dD][eE][sS][kK][tT][oO][pP])
+    homeSymArrayCall APPSINCLUDED INTERNALFILENAMEARRAYDESKTOP DESTINATIONFILENAMEARRAY
+    echo "done"
+    ;;
+    [lL][aA][pP][tT][oO][pP])
+    homeSymArrayCall APPSINCLUDED INTERNALFILENAMEARRAYLAPTOP DESTINATIONFILENAMEARRAY
+    ;;
+    *)
+    echo "Not a valid option"
+    ;;
+    esac
